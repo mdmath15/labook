9f97fe978b65049a2012eb6553772ea7460eeeb3
Matheus Ribeiro <matheuslimaribeiro15@outlook.com>
2022-06-13 12:37:41 -0300

    heroku

--- src/app.ts
+++ src/app.ts
@@ -13,9 +13,9 @@ app.use('/post', postRouter)
 
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
-    const address = server.address() as AddressInfo
-    console.log(`Server is running in http://localhost:${address.port}`)
+     const address = server.address() as AddressInfo;
+     console.log(`Server is running in http://localhost:${address.port}`);
   } else {
-    console.error(`Failure upon starting server.`)
+     console.error(`Failure upon starting server.`);
   }
 })
