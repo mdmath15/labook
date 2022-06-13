aa13ca318b6981f857c5aefa38fc263c625a94d0
Matheus Ribeiro <matheuslimaribeiro15@outlook.com>
2022-06-13 11:33:09 -0300

    heroku

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import express, { Express} from 'express'
 import cors from 'cors'
 import { AddressInfo } from 'net'
-
+import { userRouter } from './routes/user-router'
+import { postRouter } from './routes/post-router'
 
 export const app: Express = express()
 app.use(express.json())
 app.use(cors())
 
+app.use('/user', userRouter)
+app.use('/post', postRouter)
 
 const server = app.listen(process.env.PORT || 3003, () => {
   if (server) {
--- src/data/connection-db.ts
+++ src/data/connection-db.ts
@@ -12,7 +12,7 @@ export abstract class ConnectionDatabase {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_SCHEMA,
-      port: process.env.DB_PORT || 3306,
+      port: 3306,
       multipleStatements: true,
     },
   });
